3acdaab0908b3220d378010344187b00b7bf973f
Joshua George <horheyinc8@gmail.com>
2021-04-03 18:59:24 +0200

    rock, pokeball and movement started

--- script.js
+++ script.js
@@ -2,20 +2,10 @@
 let canvas = document.querySelector('canvas')
 canvas.style.backgroundColor = "blue"
 canvas.style.border = '8px solid grey'
-
 canvas.style.marginTop = '-300px'
 
 //  Paintbrush
 let ctx = canvas.getContext('2d')
-
-
-
-
-// How to load images
-// let bg = document.createElement('img)
-// let bg = new Image()
-// bg.src = './images/bg.png
-
 
 //  Buttons
 let startBtn = document.querySelector('#start')
@@ -25,7 +15,31 @@ let restartBtn = document.querySelector('#restart')
 let bg = new Image()
 bg.src = './background.jpeg'
 
-//forground
+// Pikacu
+let pika = new Image()
+pika.src = './pikachu.png'
+
+// Big rock
+let rock = new Image()
+rock.src = './rock.png'
+
+// Ball
+let ball = new Image()
+ball.src = './pngkit_pokeball-icon-png_2017019.png'
+
+let title = document.querySelector('#title')
+let gameOver = document.querySelector('#gameOver')
+let isGameOver= false;
+let intervalId = 0
+let score = 0
+
+
+// How to load images
+// let bg = document.createElement('img)
+// let bg = new Image()
+// bg.src = './images/bg.png
+
+//Foreground
 function drawSand(){
     ctx.beginPath()
     ctx.fillStyle = '#f2be7e'
@@ -38,6 +52,35 @@ function drawWave(){
 ctx.beginPath()
 ctx.fillStyle = 'blue'
 ctx.fillRect(0,200, canvas.width, 300)
+ctx.closePath()
+
+//white lines for wave
+ctx.beginPath()
+ctx.strokeStyle = 'white'
+ctx.moveTo (500, 400)
+ctx.lineTo (300, 300)
+ctx.stroke()
+ctx.closePath()
+
+ctx.beginPath()
+ctx.strokeStyle = 'white'
+ctx.moveTo (700, 400)
+ctx.lineTo (500, 300)
+ctx.stroke()
+ctx.closePath()
+
+ctx.beginPath()
+ctx.strokeStyle = 'white'
+ctx.moveTo (800, 500)
+ctx.lineTo (600, 400)
+ctx.stroke()
+ctx.closePath()
+
+ctx.beginPath()
+ctx.strokeStyle = 'white'
+ctx.moveTo (400, 500)
+ctx.lineTo (200, 400)
+ctx.stroke()
 ctx.closePath()
 
 // big block of white
@@ -170,29 +213,37 @@ ctx.arc(20,500,20,0,Math.PI*2)
 ctx.fill()
 ctx.closePath()
 
+//black lines down
+ctx.beginPath()
+ctx.strokeStyle = 'black'
+ctx.moveTo (50, 300)
+ctx.lineTo (50, 450)
+ctx.stroke()
+ctx.closePath()
 
+ctx.beginPath()
+ctx.strokeStyle = 'black'
+ctx.moveTo (75, 250)
+ctx.lineTo (75, 400)
+ctx.stroke()
+ctx.closePath()
 
-
+ctx.beginPath()
+ctx.strokeStyle = 'black'
+ctx.moveTo (20, 220)
+ctx.lineTo (20, 400)
+ctx.stroke()
+ctx.closePath()
 
 }
-
-// Pikacu
-let pika = new Image()
-pika.src = './pikachu.png'
-
-let title = document.querySelector('#title')
-let gameOver = document.querySelector('#gameOver')
-let isGameOver= false;
-let intervalId = 0
-let score = 0
      
-
 function start() {
     ctx.drawImage(bg, 0, 0, canvas.width, 400)
-
     //foreground
     drawSand()
     drawWave()
+    ctx.drawImage(rock, 500, 350, 300, 200)
+    ctx.drawImage(ball, 750, 190, 70, 70)
     ctx.drawImage(pika, 80, 250, 90, 150)
     canvas.style.display = 'block'
     startBtn.style.display = 'none'
