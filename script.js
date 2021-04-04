dc9557b4bc6c6ce51fd13d6eaef8b526fe82aba5
Joshua George <horheyinc8@gmail.com>
2021-04-04 19:21:59 +0200

    movement of rocks, bird and ball added

--- script.js
+++ script.js
@@ -27,6 +27,13 @@ rock.src = './rock.png'
 let ball = new Image()
 ball.src = './pngkit_pokeball-icon-png_2017019.png'
 
+//bird 
+let bird = new Image()
+bird.src = './PixelArt.png'
+
+// Pika falling amount
+let pikaFall = 1.5, pikaX = 70, pikaY = 200;
+
 let title = document.querySelector('#title')
 let gameOver = document.querySelector('#gameOver')
 let rockX = 500
@@ -34,6 +41,8 @@ let ballX = 750
 let isGameOver= false;
 let intervalId = 0
 let score = 0
+// Speed of objects
+let speed = 1
 
 
 // How to load images
@@ -238,17 +247,79 @@ ctx.stroke()
 ctx.closePath()
 
 }
+
+// Rock starting 
+let rockMovement = [
+    {x: canvas.width, y:canvas.height - 250},
+    {x: canvas.width + 1000, y: canvas.height - 250}
+]
+
+// Bird starting
+let birdMovement = [
+    {x:canvas.width+2500, y:120},
+    {x:canvas.width+3500, y:200}
+]
+
+// Pokeball starting
+let ballMovement = [
+    {x:canvas.width+4200, y:600},
+    {x:canvas.width+500, y:800}
+]
+
+// Pikachu pumping on the wave
+document.addEventListener('mousedown' , () => {
+    pikaFall =  pikaFall * -1
+})
+document.addEventListener('mouseup' , () => {
+    pikaFall = 1.5
+})
      
 function start() {
     ctx.drawImage(bg, 0, 0, canvas.width, 400)
+
     //foreground
     drawSand()
     drawWave()
-    ctx.drawImage(rock, rockX, 350, 300, 200)
-    ctx.drawImage(ball, ballX, 190, 70, 70)
-    ctx.drawImage(pika, 80, 250, 90, 150)
-    rockX = rockX - 2
-    ballX = ballX - 4
+
+//rock movement
+    for(let i=0; i< rockMovement.length; i++){
+        ctx.drawImage(rock, rockMovement[i].x, rockMovement[i].y, 300, 200) 
+
+        rockMovement[i].x = rockMovement[i].x - speed * 2 
+
+        if(rockMovement[i].x + rock.width == 60){
+            score ++
+        }
+    }
+
+    //bird movement
+    for(let i = 0; i < birdMovement.length; i++){
+        ctx.drawImage(bird, birdMovement[i].x, birdMovement[i].y, 120, 120) 
+
+        birdMovement[i].x = birdMovement[i].x - speed * 3
+
+        if(birdMovement[i].x + bird.width == 60){
+            score ++
+        }
+    }
+
+    //Pokeball movement
+    for(let i = 0; i < ballMovement.length; i++){
+        ctx.drawImage(ball, ballMovement[i].x, ballMovement[i].y, 120, 120) 
+
+        ballMovement[i].x = ballMovement[i].x - speed * 5
+        ballMovement[i].y = ballMovement[i].y - .3
+        
+        if(ballMovement[i].x + ball.width == 60){
+            score ++
+        }
+    }
+
+    if(score > 5){
+        speed = speed + 1
+    }
+   
+    ctx.drawImage(pika, pikaX, pikaY, 90, 90)
 
     canvas.style.display = 'block'
     startBtn.style.display = 'none'
@@ -258,8 +329,11 @@ function start() {
     // scoreboard
     ctx.font = '40px Georgia'
     ctx.fillStyle = 'black'
-    ctx.fillText(`Score is: ${score}`, canvas.width / 2 - 100, canvas.height - 30)
+    ctx.fillText(`Score is: ${score}`, canvas.width / 2 - 100, canvas.height - 20)
 
+    // Pikachu falling
+    pikaY = pikaY + pikaFall
+    
     //Game Over
     if(isGameOver){
         cancelAnimationFrame(intervalId)
@@ -267,8 +341,6 @@ function start() {
     else {
         intervalId = requestAnimationFrame(start)
     }
-
-
 }
 
 function restart(){
